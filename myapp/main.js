ea92b66428bd07d50cb5872f65e5188923b99dfc
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2023-12-07 13:49:07 +0530

    s

--- myapp/main.js
+++ myapp/main.js
@@ -1,40 +1,46 @@
-const express = require('express');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const xsenv = require('@sap/xsenv');
-const JWTStrategy = require('@sap/xssec').JWTStrategy;
-
-const users = require('./users.json');
+const express = require("express");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const xsenv = require("@sap/xsenv");
+const JWTStrategy = require("@sap/xssec").JWTStrategy;
+const fs = require("fs");
+const users = require("./users.json");
 const app = express();
 
-const services = xsenv.getServices({ uaa: 'nodeuaa' });
+const services = xsenv.getServices({ uaa: "nodeuaa" });
 
 passport.use(new JWTStrategy(services.uaa));
 
 app.use(bodyParser.json());
 app.use(passport.initialize());
-app.use(passport.authenticate('JWT', { session: false }));
+app.use(passport.authenticate("JWT", { session: false }));
 
-app.get('/users', function (req, res) {
-  var isAuthorized = req.authInfo.checkScope('$XSAPPNAME.Display');
+app.get("/users", function (req, res) {
+  var isAuthorized = req.authInfo.checkScope("$XSAPPNAME.Display");
   if (isAuthorized) {
     res.status(200).json(users);
   } else {
-    res.status(403).send('Forbidden');
+    res.status(403).send("Forbidden");
   }
 });
 
-app.post('/users', function (req, res) {
-  const isAuthorized = req.authInfo.checkScope('$XSAPPNAME.Update');
+app.post("/users", function (req, res) {
+  const isAuthorized = req.authInfo.checkScope("$XSAPPNAME.Update");
   if (!isAuthorized) {
-    res.status(403).json('Forbidden');
+    res.status(403).json("Forbidden");
     return;
   }
 
   var newUser = req.body;
   newUser.id = users.length;
   users.push(newUser);
-
+  fs.writeFile(users, newUser, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("success.");
+    }
+  });
   res.status(201).json(newUser);
 });
 // app.use(express.json())
@@ -50,5 +56,5 @@ app.post('/users', function (req, res) {
 // })
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('myapp listening on port ' + port);
+  console.log("myapp listening on port " + port);
 });
